b831367cb2d441b0f945355925194e93364847ed
drorweiss <dror@laptop>
2012-07-04 22:50:05 +0300

    update

--- app.js
+++ app.js
@@ -4,30 +4,33 @@
  */
 
 var express = require('express')
-  , routes = require('./routes')
-  , http = require('http');
+    , routes = require('./routes');
 
-var app = express.createServer();
+var app = module.exports = express.createServer();
+
+// Configuration
 
 app.configure(function(){
-  app.set('port', process.env.PORT || 8080);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(__dirname + '/public'));
 });
-/*
+
 app.configure('development', function(){
-  app.use(express.errorHandler());
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
-*/
+
+app.configure('production', function(){
+    app.use(express.errorHandler());
+});
+
+// Routes
+
 app.get('/', routes.index);
-app.listen(process.env.PORT);
-/*
-http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
-});*/
+
+app.listen(3000, function(){
+    console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+});
