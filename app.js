716f8e393c38a4eab18507ce19465c5d29fc4c89
drorweiss <dror@laptop>
2012-07-04 15:30:07 +0300

    start

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.configure('development', function(){
 });
 */
 app.get('/', routes.index);
-app.listen();
+app.listen(app.get('port'));
 /*
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
